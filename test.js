844829aee5c20ecac0f258ad5f62a2f3e9e7297c
Mathieu Gorichon <mathieu.gorichon@gmail.com>
2016-05-26 21:45:02 +0200

    test fixed with mockery

--- test.js
+++ test.js
@@ -1,9 +1,22 @@
 var assert = require('chai').assert;
-var res = require('./resource');
+var mockery = require('mockery');
 
 describe('unit test', function() {
     it('should not called azure', function() {
+        mockery.enable();
+
+        var azureMock = {
+             create : function() {
+                 console.log('mock called');
+                 return true;
+             }
+        };
+        mockery.registerMock('./azureRepo', azureMock);
+
+        var res = require('./resource');
+
         var success = res.post('mon fichier');
         assert.equal(success, true);
     });
 });
+
